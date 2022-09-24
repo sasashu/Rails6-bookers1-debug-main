dd25fb2bc9ed6a7999879c757541df241ea5cadd
EC2 Default User <ec2-user@ip-172-31-38-185.ap-northeast-1.compute.internal>
2022-09-24 07:34:33 +0000

    [Update]RSpec設定

--- db/migrate/20220922093444_add_title_to_books.rb
+++ db/migrate/20220922093444_add_title_to_books.rb
@@ -1,5 +1,4 @@
 class AddTitleToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_column :books, :title, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 2022_09_22_093444) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
